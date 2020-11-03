48098f9e0e76d42bbe70083beb849b1c03cae2f1
unknown <nak-26@mail.ru>
2020-11-04 00:03:46 +0600

    Updates

---
+++ precache-manifest.596354f3359f0f17aa2664037bccf954.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f06e722ad81b6b2b3f063eb0cee391",
+    "url": "/kcell_api/index.html"
+  },
+  {
+    "revision": "87bc7d3f6f32d318f764",
+    "url": "/kcell_api/static/css/2.19a5b00c.chunk.css"
+  },
+  {
+    "revision": "67cde6cc02f8828e5fc7",
+    "url": "/kcell_api/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "87bc7d3f6f32d318f764",
+    "url": "/kcell_api/static/js/2.cab7c87a.chunk.js"
+  },
+  {
+    "revision": "6a5dc4f543a30492c51b781c78f9b493",
+    "url": "/kcell_api/static/js/2.cab7c87a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67cde6cc02f8828e5fc7",
+    "url": "/kcell_api/static/js/main.e0e1ecaf.chunk.js"
+  },
+  {
+    "revision": "4567cd9a350c19d85c20",
+    "url": "/kcell_api/static/js/runtime-main.93b939bc.js"
+  }
+]);
